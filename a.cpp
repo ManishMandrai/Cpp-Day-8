80405ea0b1487b90ab257308a57e98b3bb020956
ManishMandrai <manishkumarmandrai@gmail.com>
2024-09-10 04:45:43 +0530

    .

--- a.cpp
+++ a.cpp
@@ -1,30 +1,33 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-int32_t main(){
+int main() {
     int n, m;
     cin >> n >> m;
     int target;
     cin >> target;
     int mat[n][m];
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             cin >> mat[i][j];
         }
     }
-    bool found = false;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            if (mat[i][j] == target)
+
+    bool found = false; 
+    for (int i = 0; i < n && !found; i++) {
+        for (int j = 0; j < m; j++) {
+            if (mat[i][j] == target) {
                 found = true;
+                break; 
+            }
         }
     }
+
     if (found)
         cout << "Found";
     else
         cout << "Not Found";
+
+    return 0; 
 }
